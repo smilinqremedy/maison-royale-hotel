e96354ed29be80f0633a4cbb32df9db38e1508d9
Smilinqremedy <ochechefredrick23@gmail.com>
2026-09-09 11:03:01 +0100

    Configure server port for deployment

--- main.go
+++ main.go
@@ -1456,11 +1456,15 @@ func main() {
 		}
 	})
 
-	log.Println(
-		"Hotel platform running at http://localhost:8081",
-	)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+	}
 
-	log.Fatal(
-		http.ListenAndServe(":8081", nil),
-	)
+	log.Printf("Hotel platform running on port %s", port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
